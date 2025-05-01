0eb518e914c1ff5501d6183493d250a468f42409
Yuka O'oka <homeyuka@gmail.com>
2025-05-01 13:34:07 +0900

    emend 08. basics

--- 08-basics/04-effect/src/components/Timer.class.tsx
+++ 08-basics/04-effect/src/components/Timer.class.tsx
@@ -49,14 +49,14 @@ class Timer extends Component<Props, State> {
 
   render() {
     return (
-      <Card className="w-80 shadow-md">
-        <CardHeader className="pt-4 pb-2">
+      <Card className="w-80 shadow-md gap-2">
+        <CardHeader>
           <div className="text-xl font-medium text-center">Count</div>
         </CardHeader>
-        <CardContent className="py-1 flex justify-center">
+        <CardContent className="flex justify-center pb-2">
           <div className="text-4xl font-semibold">{this.state.countLeft}</div>
         </CardContent>
-        <CardContent className="flex pt-4 pb-6 mx-4">
+        <CardContent className="flex mx-4">
           <Button
             className="w-full bg-red-500 hover:bg-red-600"
             onClick={this.reset}
